45f842e34a83da2026c6cadf9029ee8e17eb78c0
cswhb <u201414805@hust.edu.cn>
2018-01-22 18:40:02 +0800

    Add files via upload

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+PINPATH=/home/cswhb/pin_kit
+#PINPATH=/home/cswhb/HSCC-master/zsim-nvmain/pin_kit
+NVMAINPATH=/home/cswhb/HSCC-master/zsim-nvmain/nvmain
+ZSIMPATH=/home/cswhb/HSCC-master/zsim-nvmain
+BOOST=/home/cswhb/boost_1_59_0
+HDF5=/home/cswhb/hdf5-1.10.1/hdf5
+LIBCONFIG=/home/cswhb/libconfig-1.7.2
+#SIMPOINT_DIR=path of simpoint
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PINPATH/intel64/lib:$PINPATH/intel64/runtime:$PINPATH/intel64/lib:$PINPATH/intel64/lib-ext:$BOOST/stage/lib:$HDF5/lib:/usr/local/gmp-4.3.2/lib:/usr/local/mpfr-2.4.2/lib:/usr/local/mpc-0.8.1/lib
+INCLUDE=$INCLUDE:$HDF5/include
+LIBRARY_PATH=$LIBRARY_PATH:$HDF5/lib
+CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$HDF5/include
+export ZSIMPATH PINPATH NVMAINPATH LD_LIBRARY_PATH BOOST CPLUS_INCLUDE_PATH LIBRARY_PATH SIMPOINT_DIR
