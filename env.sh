4168c8049fc56f0b67c535d01de678afc1cfc33c
cswhb <846538430@qq.com>
2018-01-22 19:45:59 +0800

    update(software): rectify isntallation

--- env.sh
+++ env.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-PINPATH=/home/cswhb/pin_kit
-#PINPATH=/home/cswhb/HSCC-master/zsim-nvmain/pin_kit
-NVMAINPATH=/home/cswhb/HSCC-master/zsim-nvmain/nvmain
-ZSIMPATH=/home/cswhb/HSCC-master/zsim-nvmain
-BOOST=/home/cswhb/boost_1_59_0
-HDF5=/home/cswhb/hdf5-1.10.1/hdf5
-LIBCONFIG=/home/cswhb/libconfig-1.7.2
-#SIMPOINT_DIR=path of simpoint
+# PINPATH=~/pin_kit
+PINPATH=~/HSCC/zsim-nvmain/pin_kit
+NVMAINPATH=~/HSCC/zsim-nvmain/nvmain
+ZSIMPATH=~/HSCC/zsim-nvmain
+BOOST=~/boost_1_59_0
+HDF5=~/hdf5-1.10.1/hdf5
+LIBCONFIG=~/libconfig-1.7.2
+
+# SIMPOINT_DIR=path of simpoint
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PINPATH/intel64/lib:$PINPATH/intel64/runtime:$PINPATH/intel64/lib:$PINPATH/intel64/lib-ext:$BOOST/stage/lib:$HDF5/lib:/usr/local/gmp-4.3.2/lib:/usr/local/mpfr-2.4.2/lib:/usr/local/mpc-0.8.1/lib
 INCLUDE=$INCLUDE:$HDF5/include
 LIBRARY_PATH=$LIBRARY_PATH:$HDF5/lib
--- software.sh
+++ software.sh
@@ -6,17 +6,27 @@ stty -echo
 read -p "[sudo] password : " password
 stty echo
 
-# install gcc gcc-c++ autoconf
+# install gcc gcc-c++
 echo $password | sudo -S yum install -y gcc gcc-c++
 
-# install autoconf
-curl -L -O http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
-chmod +x autoconf-2.69.tar.gz
-tar -zxvf autoconf-2.69.tar.gz
-cd autoconf-2.69
-echo $password | sudo -S yum install -y openssl-devel
-./configure
-make & make install
+stty -echo
+read -p "gcc installation complete" tmp
+stty echo
+
+# install autoconf automake autogen
+# curl -L -O http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
+# chmod +x autoconf-2.69.tar.gz
+# tar -zxvf autoconf-2.69.tar.gz
+# cd autoconf-2.69
+# echo $password | sudo -S yum install -y openssl-devel
+# ./configure
+# make
+# echo $password | sudo -S make install
+echo $password | sudo -S yum install -y autoconf automake autogen libtool
+
+stty -echo
+read -p "autoconf installation complete" tmp
+stty echo
 
 # install glog
 git clone https://github.com/google/glog.git
@@ -27,18 +37,30 @@ make
 echo $password | sudo -S make install
 cd ..
 
+stty -echo
+read -p "glog installation complete" tmp
+stty echo
+
 # install python
 echo $password | sudo -S yum install -y python-devel
+
+stty -echo
+read -p "python installation complete" tmp
+stty echo
 
 # install boost
 wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz
 chmod +x boost_1_59_0.tar.gz 
 tar -zxvf boost_1_59_0.tar.gz 
 cd boost_1_59_0
-./bootstrap.sh 
+./bootstrap.sh
 ./bjam
 echo $password | sudo -S ./bjam install
 cd ..
+
+stty -echo
+read -p "boost installation complete" tmp
+stty echo
 
 # install hdf5
 wget https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.1/src/hdf5-1.10.1.tar.gz
@@ -50,6 +72,10 @@ make
 echo $password | sudo -S make install
 cd ..
 
+stty -echo
+read -p "hdf5 installation complete" tmp
+stty echo
+
 # install libconfig
 wget https://hyperrealm.github.io/libconfig/dist/libconfig-1.7.2.tar.gz
 chmod +x libconfig-1.7.2.tar.gz
@@ -59,3 +85,7 @@ cd libconfig-1.7.2
 make
 echo $password | sudo -S make install
 cd ..
+
+stty -echo
+read -p "libconfig installation complete" tmp
+stty echo
