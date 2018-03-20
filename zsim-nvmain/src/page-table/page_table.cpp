4678106cebcd1f975a0f7f70364f3c4c1e15ea16
cswhb <846538430@qq.com>
2018-03-20 15:46:52 +0800

    BFS 4MB page

--- zsim-nvmain/src/page-table/page_table.cpp
+++ zsim-nvmain/src/page-table/page_table.cpp
@@ -828,8 +828,8 @@ LongModePaging::LongModePaging(PagingStyle select): mode(select),cur_pdp_num(0),
   assert(zinfo);
   if(select == LongMode_Normal)			//4KB
   {
-	zinfo->page_size = 1024*power(2,10);
-	zinfo->page_shift = 20;
+	zinfo->page_size = 4096*power(2,10);
+	zinfo->page_shift = 22;
   }
   else if(select == LongMode_Middle )			//2MB
   {
