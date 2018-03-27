1d027e7b0fa021ba639dff4c22d00a5f6cf52756
cswhb <846538430@qq.com>
2018-03-27 22:58:23 +0800

    BFS 2MB 4*10^11 8GB

--- zsim-nvmain/double.sh
+++ zsim-nvmain/double.sh
@@ -1,9 +1,9 @@
-sh run.sh
+ehco "whb" | sudo -s sh run.sh
 git add .
-git commit -m "BFS 512KB 4*10^11 8GB"
+git commit -m "BFS 2MB 4*10^11 8GB"
 git push 
 cp ../double.cpp src/page-table/page_table.cpp
 source env.sh
 scons -c
 scons -j16
-sh run.sh
+echo "whb" | sudo -s sh run.sh
