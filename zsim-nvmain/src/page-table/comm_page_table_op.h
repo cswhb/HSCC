3ed109e5b35298784a19b375cae295bf6dc32431
cswhb <u201414805@hust.edu.cn>
2018-05-05 03:35:26 +0800

    debug

--- zsim-nvmain/src/page-table/comm_page_table_op.h
+++ zsim-nvmain/src/page-table/comm_page_table_op.h
@@ -59,7 +59,7 @@ inline void invalidate_page( PageTable* table , unsigned entry_id)
 		(*table)[entry_id]->invalidate_page();
 }
 
-//template<class T>  cswhb modified
+template<class T>  cswhb modified
 inline void invalidate_entry(PageTable* table , unsigned entry_id)
 {
 	((*table)[entry_id])->invalidate_page_table<T>();	
